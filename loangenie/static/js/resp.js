c59d5e5209ebba155d122c6bf6681d99e4063a65
RychidM <Richardsk2@gmail.com>
2022-06-24 11:44:12 +0000

    version 2

--- loangenie/static/js/resp.js
+++ loangenie/static/js/resp.js
@@ -1,18 +1,30 @@
 const hamburger = document.querySelector(".hamburger"),
       burger_menu = document.querySelector(".burger-menu"),
+      icns = document.querySelectorAll(".icns"),
       burger_close = document.querySelector(".hamburger-close");
 
 
+hamburger.addEventListener('click', ()=>{
+    burger_menu.classList.toggle('tog');
 
-hamburger.addEventListener("click", ()=>{
-    burger_menu.style.top = '0';
-    burger_menu.style.display = 'block';
-    hamburger.style.display = 'none';
-    burger_close.style.display = 'block';
-});
+    icns.forEach(icon =>{
+        icon.classList.toggle("uil-times");
+    })
+});      
 
-burger_close.addEventListener("click", ()=>{
-    hamburger.style.display = 'block';
-    burger_menu.style.top = '-450px';
-    burger_close.style.display = 'none';
-});
+
+
+// hamburger.addEventListener("click", ()=>{
+//     burger_menu.classList.toggle("tog");
+//     // burger_menu.style.top = '0';
+//     hamburger.style.display = 'none';
+//     // burger_close.style.display = 'block';
+// });
+
+// burger_close.addEventListener("click", ()=>{
+//     burger_close.classList.toggle('togg');
+//     burger_menu.classList.remove('tog');
+//     // hamburger.style.display = 'block';
+//     // burger_menu.style.top = '-450px';
+//     // burger_close.style.display = 'none';
+// });
